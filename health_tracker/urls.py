df2b962765f678a696cc845779d62491b16b53ef
Fedoseykina_Daria <fedoseykina-daria@mail.ru>
2025-10-12 20:26:00 +0500

    Add data output

--- health_tracker/urls.py
+++ health_tracker/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.HomePage, name = 'HomePage'),
     path('form/', views.HealthForm, name = 'HealthForm'),
-    path('file/', views.UploadedFileForm, name = "UploadedFileForm")
+    path('file/', views.UploadedFileForm, name = "UploadedFileForm"),
+    path('show/<str:name>/', views.JSONInfo, name = 'JSONInfo')
 ]
--- health_tracker/views.py
+++ health_tracker/views.py
@@ -15,7 +15,9 @@ def SaveUserData(data):
     folder_path = os.path.join(settings.BASE_DIR, 'Health')
     os.makedirs(folder_path, exist_ok = True)
 
-    file_path = os.path.join(folder_path, 'health.json')
+    file_name = FileName('health_note', '.json')
+
+    file_path = os.path.join(folder_path, file_name)
     if not os.path.exists(file_path):
         with open(file_path, 'w') as f:
             json.dump([], f)
@@ -40,22 +42,22 @@ def HealthForm(request):
     return render(request, 'health_tracker/health_form.html', {'form': form})
 
 #Загрузка списка записей на домашнюю страницу
-def HealthList(request):
-    file_path = os.path.join(settings.BASE_DIR, 'Health', 'health.json')
-    health_notes = []
+#def HealthList(request):
+#    file_path = os.path.join(settings.BASE_DIR, 'Health', 'health.json')
+#    health_notes = []
 
-    if os.path.exists(file_path):
-        with open(file_path, 'r') as f:
-            health_notes = json.load(f)
+#    if os.path.exists(file_path):
+#        with open(file_path, 'r') as f:
+#            health_notes = json.load(f)
     
-    return render(request, 'health_tracker/health_home.html', {'health_notes': health_notes})
+#    return render(request, 'health_tracker/health_home.html', {'health_notes': health_notes})
 
 #Зарузка формы загрузки файла
 def UploadedFileForm(request):
     if request.method == 'POST':
         form = UploadFile(request.POST, request.FILES)
         if form.is_valid():
-            HandleUploadedFile(form.cleaned_data['file'])
+            HandleUploadedFile(form.cleaned_data['title'], form.cleaned_data['file'])
             return HttpResponseRedirect('/')
 
     else:
@@ -64,17 +66,9 @@ def UploadedFileForm(request):
     return render(request, 'health_tracker/health_files.html', {'form': form})
 
 #Запись файла на сервер
-def HandleUploadedFile(f):
-    name = f.name
-    ext = ''
-
-    if '.' in name:
-        ext = name[name.rindex('.'):]
-        name = name[:name.rindex('.')]
-
-    unique_name = str(uuid.uuid4())
-    with open(f"Health/{name}_{unique_name}{ext}", "wb+") as destination:
-        for chunk in f.chunks():
+def HandleUploadedFile(title, file):
+    with open(f"Health/{FileName(title, file)}", "wb+") as destination:
+        for chunk in file.chunks():
             destination.write(chunk)
 
 #Загрузка домашней страницы
@@ -87,3 +81,33 @@ def HomePage(request):
             files.append(file)
 
     return render(request, 'health_tracker/health_home.html', {'files': files})
+
+def JSONInfo(request, name):
+    file_path = os.path.join(settings.BASE_DIR, 'Health', name)
+
+    if os.path.exists(file_path):
+        try:
+            with open(file_path, 'r') as f:
+                data = json.load(f)
+            return render(request, 'health_tracker/json_info.html', {'name': name, 'data': data})
+        except:
+            return render(request, 'health_tracker/json_info.html', {'name': name, 'error': 'Ошибка чтения'})
+    else:
+        return render(request, 'health_tracker/json_info.html', {'name': name, 'error': 'Файл не найден'})
+
+#Генерация уникального имения для файла
+def FileName(title, file):
+    name = title
+
+    if type(file) == str:
+        ext = file
+    else:
+        filename = file.name
+        ext = ''
+
+        if '.' in filename:
+            ext = filename[filename.rindex('.'):]
+
+    unique_name = str(uuid.uuid4())
+    
+    return f"{name}_{unique_name}{ext}"
